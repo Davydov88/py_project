ffe24094b3c003b148200637d906f6bc6f4ada5f
Davyd <Davydov.atc@yandex.ru>
2023-04-13 13:13:14 +0300

    Add functions "sub_numbers" in funcs.py

--- funcs.py
+++ funcs.py
@@ -4,3 +4,11 @@ def add_numbers(x, y):
 
 result = add_numbers(50, 50)
 print(result)  # Выведет 100
+
+
+def sub_numbers(x, y):
+    return x - y
+
+
+result = sub_numbers(50, 50)
+print(result)  # Выведет 0
